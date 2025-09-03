2d222d14964e589fb231c710602b89b4f414b8dd
Agustina <agustinatgg@gmail.com>
2025-09-03 23:28:55 +0200

    feat: add logic for searching cocktails and select them

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // Es el cerebro de la aplicación, controla el estado (qué está buscando, cuáles son los resultados) y se lo pasará a otros componentes
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './components/SearchBar';
 import CocktailList from './components/CocktailList';
 import logo from './logo.svg';
@@ -13,6 +13,33 @@ function App() {
   // Estado para guardar los cócteles encontrados (será un array vacío por ahora)
   const [cocktails, setCocktails] = useState([]);
 
+  // Hook que se ejecutará cuando cambien sus dependencias
+  useEffect(() => {
+    // Definimos una función asíncrona para usar await
+    const fetchCocktails = async () => {
+      // Solo buscamos si hay algo escrito para no llamar a la API si no se necesita
+      if (searchTerm.trim() === '') {
+        setCocktails([]); // Si está vacío, limpiamos los resultados
+        return;
+      }
+
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+        );
+
+        const data = await response.json();
+        // La API devuelve { drinks: [...]} o {drinks: null} si no encuentra nada
+        setCocktails(data.drinks || []);
+      } catch (error) {
+        console.error("Error fetchinv data:", error);
+        setCocktails([]); // En caso de error, vaciamos los resultados 
+      }
+    };
+
+    fetchCocktails();
+  }, [searchTerm]);
+
   // Aquí haremos la lógica para llamar a la API más adelante
   return (
     <div className="App">
@@ -20,11 +47,12 @@ function App() {
         <h1>CocktailFinder</h1>
       </header>
       <main>
-        {/* Pasamos el estado y la función para actualizarlo al SearchBar */}
-        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        {/* Pasamos el estado y la función como props para actualizarlo al SearchBar 
+        Le damos el nombre searchTerm a la prop y le pasamos el valor*/}
+        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
 
         {/*Pasamos la lista de cócteles al componente Cocktaillist */}
-        <CocktailList cocktails={cocktails}/>
+        <CocktailList cocktails={cocktails} />
       </main>
     </div>
   );
