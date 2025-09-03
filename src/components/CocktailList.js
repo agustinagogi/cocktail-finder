2cbdd6fb175f8c51a0f4d147bae3f0457d650b1e
Agustina <agustinatgg@gmail.com>
2025-09-03 23:45:42 +0200

    feat: create cocktaillist to display every cocktail in the api

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,1 +1,24 @@
-z
+import React from 'react';
+import CocktailCard from './CocktailCard';
+
+function CocktailList({cocktails, onSelectCocktail}){
+    // Si no hay cócteles, mostramos un mensaje
+
+    if (cocktails.length === 0){
+        return <p className="no-results">No se encontraron cócteles. ¡Inténtalo de nuevo!</p>
+    }
+
+    return (
+        <div className="cocktail-list">
+            {/* Usamos .map() para crear un componente CocktailCard por cada cóctel */}
+            {cocktails.map(cocktail => (
+                <CocktailCard 
+                key={cocktail.idDrink} 
+                cocktail={cocktail}
+                onSelectCocktail={onSelectCocktail}/>
+            ))}
+        </div>
+    );
+}
+
+export default CocktailList;
